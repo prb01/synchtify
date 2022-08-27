fb27800122da7344d475a8e90189f7478176b6ce
prb01 <p.bergstroem@gmail.com>
2022-08-27 09:46:45 +0200

    added playlists & cleaned up state

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -9,7 +9,8 @@ import CreateComboPlaylist from "components/playlists/CreateComboPlaylist";
 import ListOfComboPlaylists from "components/playlists/ListOfComboPlaylists";
 import { getDifferenceInMins, spotifyLogin } from "utils/utils";
 import { addSpotifyAuth, updateSpotifyAuth } from "redux/user";
-import { fetchSpotifyPlaylists, fetchCombinedPlaylistsByUid } from "redux/spotify";
+import { fetchCombinedPlaylistsByUid } from "redux/spotify";
+import { fetchSpotifyPlaylists } from "redux/playlists";
 import { fetchSpotifyMe } from "redux/spotifyUser";
 import RefreshOverlay from "./RefreshOverlay";
 
@@ -23,22 +24,8 @@ const Dashboard = (props) => {
   const [timeoutId, setTimeoutId] = useState();
   const [refreshRequired, setRefreshRequired] = useState(false);
   const {
-    data: userData,
-    isLoaded: userIsLoaded,
-    hasErrors: userHasErrors,
-    errorMsg: userErrorMsg,
-  } = useSelector((state) => state.user);
-  const {
-    data: spotifyData,
-    isLoaded: spotifyIsLoaded,
-    hasErrors: spotifyHasErrors,
-    errorMsg: spotifyErrorMsg,
-  } = useSelector((state) => state.spotify);
-  const {
-    data: spotifyUserData,
-    isLoaded: spotifyUserIsLoaded,
-    hasErrors: spotifyUserHasErrors,
-  } = useSelector((state) => state.spotifyUser);
+    user, spotify, spotifyUser, playlists
+  } = useSelector((state) => state);
 
   const handleConnectSpotify = () => {
     spotifyLogin();
@@ -46,27 +33,27 @@ const Dashboard = (props) => {
 
   useEffect(() => {
     // if no userData, then return
-    if (!userIsLoaded) return;
+    if (!user.isLoaded) return;
 
     // trigger if spotify has been authorized (code & state comes from Spotify)
     if (code && state) {
-      dispatch(addSpotifyAuth({ uid: userData.uid, code, state, redirectURI }));
+      dispatch(addSpotifyAuth({ uid: user.data.uid, code, state, redirectURI }));
       navigate("/dashboard");
       return;
     }
 
     // Access token needs to be refreshed every 60mins, so check last time it was updated
     const lastUpdateInMins = getDifferenceInMins(
-      new Date(userData.updatedAt),
+      new Date(user.data.updatedAt),
       new Date()
     );
 
     // If updated over 60mins ago, then trigger a token refresh
-    if (userData.access_token && lastUpdateInMins > 60) {
+    if (user.data.access_token && lastUpdateInMins > 60) {
       dispatch(
         updateSpotifyAuth({
-          uid: userData.uid,
-          refresh_token: userData.refresh_token,
+          uid: user.data.uid,
+          refresh_token: user.data.refresh_token,
           redirectURI,
         })
       );
@@ -76,7 +63,7 @@ const Dashboard = (props) => {
     }
 
     // if updated less than 60mins ago, put in a setTimeout call to ask user to refresh token
-    if (userData.access_token && lastUpdateInMins < 60) {
+    if (user.data.access_token && lastUpdateInMins < 60) {
       if (timeoutId) clearTimeout(timeoutId);
 
       const timeToWaitInMs = (60 - lastUpdateInMins) * 60 * 1000;
@@ -92,28 +79,28 @@ const Dashboard = (props) => {
 
     // This should be triggered if access token exists & has been alive for less than 60mins
     // If true, then get Spotify details for user & fetch their Spotify playlists
-    if (userData.access_token) {
-      dispatch(fetchSpotifyMe({ access_token: userData.access_token })).then(
+    if (user.data.access_token) {
+      dispatch(fetchSpotifyMe({ access_token: user.data.access_token })).then(
         (data) => {
           dispatch(
             fetchSpotifyPlaylists({
               user: data.payload,
-              access_token: userData.access_token,
+              access_token: user.data.access_token,
             })
           );
 
           // fetch their combined Playlists as well from DB
-          dispatch(fetchCombinedPlaylistsByUid({ uid: userData.uid }));
+          dispatch(fetchCombinedPlaylistsByUid({ uid: user.data.uid }));
         }
       );
     }
-  }, [userIsLoaded, userData]);
+  }, [user.isLoaded, user.data]);
 
   const handleRefreshToken = () => {
     dispatch(
       updateSpotifyAuth({
-        uid: userData.uid,
-        refresh_token: userData.refresh_token,
+        uid: user.data.uid,
+        refresh_token: user.data.refresh_token,
         redirectURI,
       })
     );
@@ -124,13 +111,13 @@ const Dashboard = (props) => {
   return (
     <div className="vw-100 min-vh-100 h-100 d-flex flex-column align-items-center homepage-bg p-2 pt-5 text-text">
       <Nav />
-      {!userIsLoaded && (
+      {!user.isLoaded && (
         <Spinner color="secondary" className="position-absolute top-50 start-50">
           Loading...
         </Spinner>
       )}
-      {userHasErrors && "Error Loading user data..."}
-      {userIsLoaded && !userHasErrors && !userData.access_token && (
+      {user.hasErrors && "Error Loading user data..."}
+      {user.isLoaded && !user.hasErrors && !user.data.access_token && (
         <div className="position-absolute top-50 start-50 translate-middle d-flex flex-column justify-content-center align-items-center overflow-hidden">
           <h1 className="text-text mb-3">Connect your Spotify Account to start</h1>
           <div className="d-flex justify-content-center">
@@ -145,16 +132,16 @@ const Dashboard = (props) => {
           </div>
         </div>
       )}
-      {userIsLoaded && !userHasErrors && userData.access_token && (
+      {user.isLoaded && !user.hasErrors && user.data.access_token && (
         <>
           {refreshRequired && (
             <RefreshOverlay handleRefreshToken={handleRefreshToken} />
           )}
           <div>
-            {!spotifyIsLoaded &&
-              !spotifyData.playlists &&
-              !spotifyUserData &&
-              !spotifyData.combinedPlaylists && (
+            {!spotify.isLoaded &&
+              !playlists.isLoaded &&
+              !spotifyUser.isLoaded &&
+              !spotify.data.combinedPlaylists && (
                 <Spinner
                   color="secondary"
                   className="position-absolute top-50 start-50"
@@ -162,12 +149,12 @@ const Dashboard = (props) => {
                   Loading...
                 </Spinner>
               )}
-            {spotifyHasErrors && `Error Loading: ${spotifyErrorMsg}`}
-            {spotifyIsLoaded && spotifyData.playlists && <CreateComboPlaylist />}
+            {spotify.hasErrors && `Error Loading...`}
+            {playlists.isLoaded && <CreateComboPlaylist />}
 
-            {spotifyIsLoaded && spotifyData.combinedPlaylists && (
+            {spotify.isLoaded && spotify.data.combinedPlaylists && (
               <ListOfComboPlaylists
-                combinedPlaylists={spotifyData.combinedPlaylists}
+                combinedPlaylists={spotify.data.combinedPlaylists}
               />
             )}
           </div>
