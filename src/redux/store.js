ea21f3b3e31a846b41c351f3acdb639122cc4bf9
prb01 <p.bergstroem@gmail.com>
2022-08-27 10:14:56 +0200

    added combinedPlaylist reducer

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,16 +1,18 @@
 // https://dev.to/thatgalnatalie/how-to-get-started-with-redux-toolkit-41e
-import { configureStore } from '@reduxjs/toolkit'
-import { combineReducers } from 'redux'
-import { reducer as user } from './user';
-import { reducer as spotify } from "./spotify"
-import { reducer as spotifyUser} from "./spotifyUser"
-import { reducer as playlist } from "./playlist"
+import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers } from "redux";
+import { reducer as user } from "./user";
+import { reducer as spotify } from "./spotify";
+import { reducer as spotifyUser } from "./spotifyUser";
+import { reducer as playlist } from "./playlist";
+import { reducer as combinedPlaylist } from "./combinedPlaylist";
 
 const reducer = combineReducers({
-    user,
-    spotify,
-    spotifyUser,
-    playlist
+  user,
+  spotify,
+  spotifyUser,
+  playlist,
+  combinedPlaylist,
 });
 
 const store = configureStore({
