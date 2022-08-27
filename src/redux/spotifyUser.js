916c09aaa55f2559ad6c2a6b2353fe89721b9927
prb01 <p.bergstroem@gmail.com>
2022-08-27 09:12:08 +0200

    added new redux store

---
+++ src/redux/spotifyUser.js
@@ -0,0 +1,49 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { cloudService } from "services/cloudService";
+
+const initialState = {
+  data: {},
+  isLoaded: false,
+  hasErrors: false,
+};
+
+const spotifyUser = createSlice({
+  name: "spotifyUser",
+  initialState,
+  reducers: {
+    getData: (state) => {},
+
+    getDataSuccess: (state, action) => {
+      state.isLoaded = true;
+      state.data = action.payload;
+    },
+
+    getDataFailure: (state, action) => {
+      state.isLoaded = true;
+      state.hasErrors = true;
+    },
+  },
+});
+
+export const reducer = spotifyUser.reducer;
+
+export const {
+  getData,
+  getDataSuccess,
+  getDataFailure,
+} = spotify.actions;
+
+export const fetchSpotifyMe = createAsyncThunk(
+  "spotifyUser/fetchSpotifyMe",
+  async (payload, thunkAPI) => {
+    try {
+      thunkAPI.dispatch(getData());
+      const data = await cloudService.getMe(payload.access_token);
+      thunkAPI.dispatch(getDataSuccess(data));
+
+      return data.id;
+    } catch (error) {
+      thunkAPI.dispatch(getDataFailure());
+    }
+  }
+);
