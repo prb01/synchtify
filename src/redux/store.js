96957e130f3416e8cf859a5ec2d6acc667309acc
prb01 <p.bergstroem@gmail.com>
2022-08-27 09:13:05 +0200

    combined spotifyUser reducer

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,10 +3,12 @@ import { configureStore } from '@reduxjs/toolkit'
 import { combineReducers } from 'redux'
 import { reducer as user } from './user';
 import { reducer as spotify } from "./spotify"
+import { reducer as spotifyUser} from "./spotifyUser"
 
 const reducer = combineReducers({
     user,
-    spotify
+    spotify,
+    spotifyUser
 });
 
 const store = configureStore({
