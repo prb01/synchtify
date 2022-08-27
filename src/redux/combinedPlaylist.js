81905aa04a5c5226a41dbd47e67690b65ec2b2c0
prb01 <p.bergstroem@gmail.com>
2022-08-27 10:15:10 +0200

    created new combinedPlaylist reducer

---
+++ src/redux/combinedPlaylist.js
@@ -0,0 +1,141 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { cloudService } from "services/cloudService";
+
+const initialState = {
+  data: {},
+  isLoaded: false,
+  hasErrors: false,
+};
+
+const combinedPlaylist = createSlice({
+  name: "combinedPlaylist",
+  initialState,
+  reducers: {
+    getData: (state) => {},
+
+    getDataSuccess: (state, action) => {
+      state.isLoaded = true;
+      state.data = action.payload;
+    },
+
+    getDataFailure: (state, action) => {
+      state.isLoaded = true;
+      state.hasErrors = true;
+    },
+    createDataSuccess: (state, action) => {
+      state.data = { ...state, ...action.payload };
+      state.isLoaded = true;
+      state.hasErrors = false;
+    },
+    createDataFailure: (state) => {
+      state.hasErrors = true;
+    },
+    removeData: (state, action) => {
+      state.data = state.data.filter(
+        (playlist) => playlist.id !== action.payload
+      );
+    },
+  },
+});
+
+export const reducer = combinedPlaylist.reducer;
+
+export const {
+  getData,
+  getDataSuccess,
+  getDataFailure,
+  createDataSuccess,
+  createDataFailure,
+  removeData,
+} = combinedPlaylist.actions;
+
+export const fetchCombinedPlaylistsByUid = createAsyncThunk(
+  "combinedPlaylist/fetchCombinedPlaylists",
+  async (payload, thunkAPI) => {
+    thunkAPI.dispatch(getData());
+
+    try {
+      const data = await _fetchCombinedPlaylistsByUidFromDb(payload.uid);
+      thunkAPI.dispatch(getDataSuccess(data));
+      return data;
+    } catch (error) {
+      thunkAPI.dispatch(getDataFailure());
+    }
+  }
+);
+
+export const createCombinedPlaylist = createAsyncThunk(
+  "combinedPlaylist/createCombinedPlaylist",
+  async (payload, thunkAPI) => {
+    try {
+      const data = await _createCombinedPlaylistInDb(
+        payload.id,
+        payload.uid,
+        payload.name,
+        payload.playlists
+      );
+
+      console.log({ data });
+
+      return payload.id;
+    } catch (error) {
+      thunkAPI.dispatch(createDataFailure());
+      return false;
+    }
+  }
+);
+
+export const deleteCombinedPlaylist = createAsyncThunk(
+  "combinedPlaylist/deleteCombinedPlaylist",
+  async (payload, thunkAPI) => {
+    try {
+      await cloudService.unfollowPlaylist(payload.id, payload.access_token);
+
+      await _deleteCombinedPlaylistFromDb(payload.id);
+      thunkAPI.dispatch(removeData(payload.id));
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
+);
+
+async function _fetchCombinedPlaylistsByUidFromDb(uid) {
+  const snapshot = await firebaseClient
+    .firestore()
+    .collection("combined_playlists")
+    .where("uid", "==", uid)
+    .get();
+
+  const data = snapshot.docs.map((doc) => ({
+    id: doc.id,
+    ...doc.data(),
+    updatedAt: doc.updatedAt?.toDate().toISOString(),
+  }));
+
+  return data;
+}
+
+async function _createCombinedPlaylistInDb(id, uid, name, playlists) {
+  const doc = await firebaseClient
+    .firestore()
+    .collection("combined_playlists")
+    .doc(id)
+    .set({
+      uid,
+      name,
+      updatedAt: firebase.firestore.FieldValue.serverTimestamp(),
+      playlists,
+    });
+
+  return doc;
+}
+
+async function _deleteCombinedPlaylistFromDb(id) {
+  const snapshot = await firebaseClient
+    .firestore()
+    .collection("combined_playlists")
+    .doc(id)
+    .delete();
+
+  return snapshot;
+}
