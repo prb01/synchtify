1dcdbd8699dac1594e1e93797ff2ec8483f956f9
prb01 <p.bergstroem@gmail.com>
2022-08-27 15:39:45 +0200

    removed spotify reducer

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,14 +2,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { combineReducers } from "redux";
 import { reducer as user } from "./user";
-import { reducer as spotify } from "./spotify";
 import { reducer as spotifyUser } from "./spotifyUser";
 import { reducer as playlist } from "./playlist";
 import { reducer as combinedPlaylist } from "./combinedPlaylist";
 
 const reducer = combineReducers({
   user,
-  spotify,
   spotifyUser,
   playlist,
   combinedPlaylist,
