10b5fe442d8bc1bf5dc4045764f351cf7677d193
prb01 <p.bergstroem@gmail.com>
2022-08-30 13:56:34 +0200

    added provisional appCheck

--- src/firebase/client.js
+++ src/firebase/client.js
@@ -1,25 +1,30 @@
 // import fb doens't work easily with mjs
 // https://stackoverflow.com/questions/42090156/firebase-initializeapp-is-not-a-function
 // https://github.com/nodejs/node/issues/33410
-import { firebase } from '@firebase/app';
+import { firebase } from "@firebase/app";
 // These imports load individual services into the firebase namespace. e.g. import 'firebase/auth';
-import '@firebase/auth';
-import '@firebase/storage';
+import "@firebase/auth";
+import "@firebase/storage";
 import "firebase/firestore";
+import "firebase/app-check";
 
 const firebaseKey = process.env.REACT_APP_FIREBASE_API_KEY;
 const firebaseProjectId = process.env.REACT_APP_FIREBASE_PROJECT_ID;
 const firebaseAuthDomain = process.env.REACT_APP_FIREBASE_AUTH_DOMAIN;
 const firebaseStorageBucket = process.env.REACT_APP_FIREBASE_STORAGE_BUCKET;
+const recaptchaKey = process.env.REACT_APP_RECAPTCHA_KEY;
 
 const options = {
-    apiKey: firebaseKey,
-    authDomain: firebaseAuthDomain,
-    projectId: firebaseProjectId,
-    storageBucket: firebaseStorageBucket,
+  apiKey: firebaseKey,
+  authDomain: firebaseAuthDomain,
+  projectId: firebaseProjectId,
+  storageBucket: firebaseStorageBucket,
 };
 
 const client = firebase.initializeApp(options);
+
+// const appCheck = firebase.appCheck(client);
+// appCheck.activate(recaptchaKey, true);
 
 export default client;
 export { firebase };
